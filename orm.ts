e4a067a645d381d5a497e18315e197669b84feb8
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-29 00:34:58 +0530

    feat: add user migrations

--- orm.ts
+++ orm.ts
@@ -11,7 +11,7 @@ export const typeOrmConfig: TypeOrmModuleOptions & DataSourceOptions = {
     username: process.env.DATABASE_USERNAME,
     password: String(process.env.DATABASE_PASSWORD),
     database: process.env.DATABASE_NAME,
-    entities: ["dist/**/*.entity{.ts,.js}"],
+    entities: ["dist/src/entities/*.entity{.ts,.js}"],
     migrations: ["dist/migrations/*{.ts,.js}"],
     synchronize: false,
 }
