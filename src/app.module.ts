ac245c365bd3f8d93f4277954a2d18cf2673eb97
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-28 22:58:40 +0530

    feat: typeorm initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '.././orm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory:async () => typeOrmConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
